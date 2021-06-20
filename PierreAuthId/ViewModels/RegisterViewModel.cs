59afa6b7951c1c07988ce090983bb067f20bc377
JuanHasbunZem <zemenarwow@gmail.com>
2021-06-19 23:26:49 -0700

    add ViewModel pages for login register

--- PierreAuthId/ViewModels/RegisterViewModel.cs
+++ PierreAuthId/ViewModels/RegisterViewModel.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PierreAuthId.ViewModels
 {
-  public class RegisterViewModels
+  public class RegisterViewModel
   {
       [Required]
       [EmailAddress]
